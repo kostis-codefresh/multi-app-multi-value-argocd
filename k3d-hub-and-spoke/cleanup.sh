8eadf03a2b32cdc0f1d0dc7d09d1fbcdff2e81cb
Kostis Kapelonis <kostis@codefresh.io>
2025-06-03 14:21:52 +0300

    Fixed cleanup script

--- k3d-hub-and-spoke/cleanup.sh
+++ k3d-hub-and-spoke/cleanup.sh
@@ -6,6 +6,6 @@ k3d cluster delete qa-eu
 k3d cluster delete qa-us
 k3d cluster delete staging
 k3d cluster delete prod-eu
-k3d cluster delete prod-eu
-k3d cluster delete prod-eu
+k3d cluster delete prod-asia
+k3d cluster delete prod-us
 k3d cluster delete admin
